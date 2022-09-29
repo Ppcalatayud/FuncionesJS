0b15bb3206fb05d3610b4ab593f3afcf64dbe4f3
Ppcalatayud <pepe2003.pc@gmail.com>
2022-09-29 09:04:37 +0200

    primer commit

---
+++ main.js
@@ -0,0 +1,94 @@
+
+// Crea un programa el cual te pregunte por una nota del 0 al 10 y dependiendo del número, te devuelva la siguiente clasificación.
+
+
+// function Minota() {
+//     let nota = prompt("Introduce tu nota del 0 al 10");
+
+
+//     switch (nota) {
+
+//         case '0':
+//         case '1':
+//         case '2':
+//         case '3':
+//         case '4':
+//             console.log("Insuficiente.")
+//             break;
+
+//         case '5':
+//         case '6':
+//             console.log("Suficiente.")
+//             break;
+
+//         case '7':
+//         case '8':
+//             console.log("Notable.")
+//             break;
+
+//         case '9':
+//         case '10':
+//             console.log("Sobresaliente.")
+//             break;
+//         default:
+//             break;
+//     }
+// return nota
+
+// }
+
+// console.log(Minota())
+
+
+
+
+
+// Crea una función resta que espere dos parámetros a y b y que devuelva la resta de los mismos.
+
+
+//     function resta() {
+//      let num1 = prompt("Primer numero de la resta");
+//      let num2 = prompt("Segundo numero de la resta");
+
+//      return num1-num2
+// }
+
+// console.log(resta())
+
+
+
+
+// Crea la función duplicaNumero debe recibir un tipo number y devolver el doble del valor recibido. Si la función no recibe un dato tipo number debe devolver el string ‘Debo ser ejecutada con un número’
+
+
+// function duplicaNumero(number) {
+    
+//          let num1 = prompt("Numero para multiplicar");
+//          if(typeof num1 == "number"){return console.log("Debe de ser un numero")}
+
+//         console.log(num1*2)
+//     }
+    
+//     (duplicaNumero())
+
+
+
+// Crea la función caracterInicial  debe recibir un tipo string y devolver un string con el primer carácter.
+
+
+
+// function caracterInicial(primerletra){
+
+//     if(typeof primerletra!=string)
+//        console.log ("Debo ser ejecutada con un string")
+
+//     else if     
+//        console.log("Debo ser ejecutada con un string no vacio")
+
+
+// }
+
+// console.log(caracterInicial(""))
+
+
+// me faltan
